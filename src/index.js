39f5eb7d2d48da278f8e06d7c20f89e8397eaf91
Jascha <jascha@jaeh.at>
2018-11-08 11:44:26 +0100

    use async await instead of promise, only load variables.styl if it exists

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const fs = require('fs')
 const util = require('util')
 const path = require('path')
 
@@ -8,18 +9,30 @@ const autoprefixer = require('autoprefixer-stylus')
 
 const log = require('@magic/log')
 
+const exists = util.promisify(fs.exists)
+
 // stylus build task
-const style = (string, config) =>
-  new Promise((resolve, reject) => {
-    stylus(string)
+const style = async (string, config) => {
+  try {
+    const style = stylus(string)
       .set('paths', [config.CSS_DIR])
       .set('sourcemap', {})
       .define('WEB_ROOT', config.ENV !== 'development' && config.WEB_ROOT ? config.WEB_ROOT : '/')
       .use(nib())
       .use(autoprefixer())
-      .import(path.join(config.CSS_DIR, 'variables'))
-      .render((err, css) => (err ? reject(err) : resolve(css)))
-  })
+
+    const varFile = path.join(config.CSS_DIR, 'variables.styl')
+    if (await exists(varFile)) {
+      style.import(varFile)
+    }
+
+    style.render = util.promisify(style.render)
+    const css = await style.render()
+    return css
+  } catch(e) {
+    throw e
+  }
+}
 
 const minify = style =>
   style
